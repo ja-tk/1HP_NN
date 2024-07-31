41a44fd763cd32b9293c751fcc2600ac14ba22d9
ja-tk <st166162@stud.uni-stuttgart.de>
2024-07-31 07:40:43 +0200

    tests adapted to dimension x

--- unittests/test_3d_1ststage.py
+++ unittests/test_3d_1ststage.py
@@ -1,23 +1,22 @@
 import torch
 import yaml
-#from unittest import Assert
 
-from postprocessing.visualization import get_hp_z_position, get_hp_z_position_from_info, remove_z_dimension
+from postprocessing.visualization import get_hp_position, get_hp_position_from_info, remove_dimension
+#to start, move file into parent folder
 
-
-def test_get_hp_z_position():
+def test_get_hp_position():
     #Fixture
     tensor=torch.zeros((5, 5, 5))
-    tensor[1,2,3] = 1
+    tensor[3,2,1] = 1
 
     # Expected result
     expected = 3
     # Actual result
-    actual=get_hp_z_position(tensor)
+    actual=get_hp_position(tensor)
     # Test
-    assert actual==expected, "Result is not the expected z position"
+    assert actual==expected, "Result is not the expected position"
 
-def test_get_hp_z_position_from_info():
+def test_get_hp_position_from_info():
     #Fixture
     with open("unittests/dummyInfo.yaml", "r") as f:
             info = yaml.safe_load(f)
@@ -29,59 +28,60 @@ def test_get_hp_z_position_from_info():
         [[0,0,0], [0,0,0], [0,0,0]],
         [[0,1,0], [0,0,0], [0,0,0]],
         [[0,0,0], [0,0,0], [0,0,0]]],
-        [#sdf
-        [[0,0,1], [0,0,0], [0,0,0]],
+        [#sdf-tensor-dummy: max at hp_position (0,1,2)
+        [[0,0,0], [0,0,1], [0,0,0]],
         [[0,0,0], [0,0,0], [0,0,0]],
         [[0,0,0], [0,0,0], [0,0,0]]],
-        [#Material ID
-        [[0,0,1], [0,0,0], [0,0,0]],
+        [#Material ID-tensor-dummy: max at hp_position (0,1,2)
+        [[0,0,0], [0,0,1], [0,0,0]],
         [[0,0,0], [0,0,0], [0,0,0]],
         [[0,0,0], [0,0,0], [0,0,0]]]])
     
     # Expected result
-    expected = 2
+    expected = 0
     # Actual result
-    actual=get_hp_z_position_from_info(tensor,info)
+    actual=get_hp_position_from_info(tensor,info)
     # Test
-    assert actual==expected, "Result is not the expected z position"
+    assert actual==expected, "Result is not the expected position"
 
-def test_remove_z_dimension_3d():
+def test_remove_dimension_3d():
     #Fixture
     #4x2x3
     tensor = torch.Tensor([
-        [[0,0,1], [0,0,1]],
-        [[0,0,1], [0,0,1]],
-        [[0,0,1], [0,0,1]],
-        [[0,0,1], [0,0,1]]])
+        [[0,0,0], [0,0,0]],
+        [[0,0,0], [0,0,0]],
+        [[1,1,1], [1,1,1]],
+        [[0,0,0], [0,0,0]]])
     
-    z_position=2
-    
-    # Expected result
-    expected = torch.ones((4,2))
-    # Actual result
-    actual=remove_z_dimension(tensor,z_position)
-    # Test
-    assert torch.allclose(actual, expected), f"z dim could not be removed at z position {z_position}"
+    position=2
 
-def test_remove_z_dimension_4d():
+    # Expected result
+    expected = torch.ones((2,3))
+    # Actual result
+    actual=remove_dimension(tensor,position)
+    # Test
+    assert torch.allclose(actual, expected), f"z dim could not be removed at position {position}"
+
+def test_remove_dimension_4d():
      #Fixture
     tensor=torch.zeros((4, 2, 3, 4))
-    tensor[:,:,:,0]=5
-    tensor[:,:,:,1]=6
-    tensor[:,:,:,2]=7
-    tensor[:,:,:,3]=8
+    tensor[:,0,:,:]=5
+    tensor[:,1,:,:]=6
 
-    z_position=0
+    position=0
     
     # Expected result
-    expected = tensor[:,:,:,0]
+    expected = tensor[:,0,:,:]
     # Actual result
-    actual=remove_z_dimension(tensor,z_position)
+    actual=remove_dimension(tensor,position)
     # Test
-    assert torch.allclose(actual, expected), f"z dim could not be removed at z position {z_position}"
+    assert torch.allclose(actual, expected), f"z dim could not be removed at position {position}"
+
 
 if __name__ == "__main__":
-    test_get_hp_z_position()
-    test_get_hp_z_position_from_info()
-    test_remove_z_dimension_3d()
-    test_remove_z_dimension_4d()
+    print("tests start")
+    test_get_hp_position()
+    test_get_hp_position_from_info()
+    test_remove_dimension_3d()
+    test_remove_dimension_4d()
+    print("tests finished")
