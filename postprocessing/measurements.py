64cc369ef8f64a0d3333d93982f56515b9c63da1
ja-tk <st166162@stud.uni-stuttgart.de>
2024-11-13 21:34:56 +0100

    added measurements of 2d planes from 3d data

--- postprocessing/measurements.py
+++ postprocessing/measurements.py
@@ -12,6 +12,7 @@ from networks.unet import UNet
 from networks.unet3d import UNet3d
 from processing.solver import Solver
 from data_stuff.utils import SettingsTraining
+from data_stuff.utils_3d import get_hp_position_from_input, remove_first_dim
 
 def measure_len_width_1K_isoline(data: Dict[str, "DataToVisualize"]):
     ''' 
@@ -84,61 +85,64 @@ def measure_loss(model: UNet|UNet3d, dataloader: DataLoader, device: str, loss_f
     return {"mean squared error": mse_loss, "mean squared error in [°C^2]": mse_closs, 
             "mean absolute error": mae_loss, "mean absolute error in [°C]": mae_closs}
 
-def measure_additional_losses(model: UNet|UNet3d, dataloader: DataLoader, summed_error_pic: torch.Tensor = None, settings: SettingsTraining = None):
-    #based on: https://github.com/JuliaPelzer/Heat-Plume-Prediction/blob/main/postprocessing/measurements.py
-    try:
-        norm = dataloader.dataset.norm
-    except AttributeError:
-        norm = dataloader.dataset.dataset.norm
-    model.eval()
-    results = {}
-    var = 0.0
 
-    for x, y in dataloader: # batchwise
-        x = x.to(settings.device).detach()
-        y = y.to(settings.device).detach()
-        y_pred = model(x).to(settings.device).detach()
-        y = torch.swapaxes(y, 0, 1)
-        y_pred = torch.swapaxes(y_pred, 0, 1)
-        y = norm.reverse(y.cpu(),"Labels")
-        y_pred = norm.reverse(y_pred.cpu(),"Labels")
-        y = torch.swapaxes(y, 0, 1)
-        y_pred = torch.swapaxes(y_pred, 0, 1)
-
-        var += torch.mean(torch.pow(abs((y_pred - y) - summed_error_pic),2))
-             
-    var /= len(dataloader)
-    std = torch.sqrt(var)
-
-    results = {"variance in [°C]": var, "standard deviation in [°C]": std}
-
-    return results
-
-def measure_losses(model: UNet|UNet3d, dataloader: DataLoader, settings: SettingsTraining):
+def measure_additional_losses(model: UNet|UNet3d, dataloader: DataLoader, settings: SettingsTraining, summed_error_pic: torch.Tensor):
+    # based on: https://github.com/JuliaPelzer/Heat-Plume-Prediction/blob/allin1_preproc_v2/postprocessing/measurements.py
     '''
-    function to measure losses: mse_loss, mae_closs, rmse_closs, r2_closs, pbt_threshold
-    based on https://github.com/JuliaPelzer/Heat-Plume-Prediction/blob/allin1_preproc_v2/postprocessing/measurements.py
+    Function to measure losses for 2D planes: mse_loss, mae_closs, rmse_closs, pat_closs, var_closs, std_closs.
+
+    In case of 3D data:
+    -cut_dim: choose "x" or "z" to specify whether to cut at the x or z height of the heat pump
+    -dim_start, dim_end: start and end point to cut the 2d plane in the remaining z or x direction
     '''
-    pbt_threshold = 0.1 # [°C]
-    
+
+    # only relevant for 3D
+    cut_dim = "x" # x or z
+    dim_start = 23 # comparing against 2D model with output 256x16
+    dim_end = 39
+
+    pat_threshold = 0.1 # [°C]
+
     device = settings.device
-    if settings.problem == "allin1":
-        norm = dataloader.dataset.norm
-    elif settings.problem in ["1hp", "2stages", "3d"]:
-        norm = dataloader.dataset.dataset.norm
+    norm = dataloader.dataset.dataset.norm
+    info = dataloader.dataset.dataset.info
     model.eval()
     results = {}
 
     mse_loss = torch.Tensor([0.0,] )
     mae_closs = torch.Tensor([0.0,] )
     rmse_closs = torch.Tensor([0.0,] )
-    r2_closs = torch.Tensor([0.0,] )
-    pbt_closs = torch.Tensor([0.0,] )
+    pat_closs = torch.Tensor([0.0,] )
+    var_closs = torch.Tensor([0.0,] )
+
+    if settings.problem=="3d":
+        loc_hp=get_hp_position_from_input(dataloader.dataset[0][0].to(device).detach(), info)
+
+        #bring cut_dim to first position
+        if cut_dim == "z":
+            summed_error_pic.transpose(0, 2)# Z,Y,X
+            loc_hp = loc_hp[::-1]
+
+        summed_error_pic = remove_first_dim(summed_error_pic, loc_hp[0])
+        summed_error_pic = summed_error_pic[:, dim_start:dim_end]
+    
 
     for x, y in dataloader:
         x = x.to(device).detach() # B,C,X,Y,Z
         y = y.to(device).detach()
         y_pred = model(x).to(device).detach()
+        
+        if settings.problem=="3d":
+            
+            if cut_dim == "z":
+                # (B,C,X,Y,Z) -> (B,C,Z,Y,X)
+                y=y.transpose(2,4)
+                y_pred=y_pred.transpose(2,4)
+                
+            y = remove_first_dim(y, loc_hp[0])
+            y_pred = remove_first_dim(y_pred, loc_hp[0])
+            y = y[:,:,:,dim_start:dim_end]
+            y_pred = y_pred[:,:,:,dim_start:dim_end]
 
         # normed losses
         mse_loss += MSELoss(reduction="sum")(y_pred, y).item()
@@ -154,38 +158,38 @@ def measure_losses(model: UNet|UNet3d, dataloader: DataLoader, settings: Setting
         # losses in Celsius
         mae_closs += L1Loss(reduction="sum")(y_pred, y).item()
         rmse_closs += MSELoss(reduction="sum")(y_pred, y).item()
-        r2_closs -= (MSELoss()(y_pred, y) / MSELoss()(y,torch.mean(y)) * y_pred.shape[0]).item()
-        
-        # count all pixels where the difference is bigger than 0.1°C, then average over number datapoints and number of cells
-        pbt_closs += (torch.sum(torch.abs(y_pred - y) > pbt_threshold)).item()
+        var_closs += torch.sum(torch.pow(abs(y_pred - y) - summed_error_pic.expand_as(y),2))
+
+        # count all pixels/voxels where the difference is above 0.1°C, then average over number datapoints and number of cells
+        pat_closs += (torch.sum(torch.abs(y_pred - y) > pat_threshold)).item()
 
     # average over all datapoints
     no_datapoints = len(dataloader.dataset)
-    if settings.problem=="3d":
-        no_cells = y_pred.shape[2] * y_pred.shape[3]*y_pred.shape[4]
-    else:
-        no_cells = y_pred.shape[2] * y_pred.shape[3]
-
-    print(f"Number of cells: {no_cells}")
-    print(f"Number of datapoints: {no_datapoints}")
+    no_cells = y_pred.shape[2] * y_pred.shape[3]
     
     mse_loss /= (no_datapoints * no_cells)
     mae_closs /= (no_datapoints * no_cells)
     rmse_closs /=  (no_datapoints * no_cells)
+    var_closs /= (no_datapoints * no_cells)
+    
     rmse_closs = torch.sqrt(rmse_closs)
-    r2_closs /= no_datapoints
-    r2_closs += 1
-    pbt_closs /= (no_datapoints * no_cells) 
-    pbt_closs *= 100 # value close to 0% is good, close to 100% is bad
+    std_closs = torch.sqrt(var_closs)
 
-    results = {"MSE [-] ": "{:.2e}".format(mse_loss.item()), f"MAE [°C] ": "{:.2e}".format(mae_closs.item()), f"RMSE [°C] ": "{:.2e}".format(rmse_closs.item()), f"R2 based on °C ": "{:.2e}".format(r2_closs.item())} 
-        
-    results["PBT (percentage bigger than threshold 0.1C in [%])"] = pbt_closs.item()
+    pat_closs /= (no_datapoints * no_cells)
+    pat_closs *= 100 # value close to 0% is good, close to 100% is bad
+
+    results["MSE [-] "] = "{:.2e}".format(mse_loss.item())
+    results["MAE [°C] "] = "{:.2e}".format(mae_closs.item())
+    results["RMSE [°C] "] = "{:.2e}".format(rmse_closs.item())
+    results[f"PAT (Percentage Above Threshold {pat_threshold}C in [%])"] = "{:.2e}".format(pat_closs.item())
+    results["variance in [°C^2]"] = "{:.2e}".format(var_closs.item())
+    results["standard deviation in [°C]"] = "{:.2e}".format(std_closs.item())
+    results["Maximum of CAE in [°C]"]="{:.2e}".format(summed_error_pic.max().item())
 
     return results
 
 
-def save_all_measurements(settings:SettingsTraining, len_dataset, times, solver:Solver=None, errors:Dict={}):
+def save_all_measurements(model: UNet|UNet3d, settings:SettingsTraining, len_dataset, times, solver:Solver=None, errors:Dict={}):
     with open(Path.cwd() / "runs" / settings.destination / f"measurements_{settings.case}.yaml", "w") as f:
         for key, value in times.items():
             f.write(f"{key}: {value}\n")
@@ -195,6 +199,7 @@ def save_all_measurements(settings:SettingsTraining, len_dataset, times, solver:
             f.write(f"duration of initializations in seconds: {times['time_initializations']-times['time_begin']}\n")
             f.write(f"duration of training in seconds: {times['time_training']-times['time_initializations']}\n")
         f.write(f"input params: {settings.inputs}\n")
+        f.write(f"number of params: {model.num_of_params()}\n")
         f.write(f"dataset location: {settings.dataset_prep.parent}\n")
         f.write(f"dataset name: {settings.dataset_raw}\n")
         f.write(f"case: {settings.case}\n")
