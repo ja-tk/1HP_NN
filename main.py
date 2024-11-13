51969aeae89de6013efcdbdac91c7c855ee28abf
ja-tk <st166162@stud.uni-stuttgart.de>
2024-11-13 20:19:18 +0100

    SmoothL1 Loss for 3D and easier handling of batchsize

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import numpy as np
 import time
 import torch
 import yaml
+from pathlib import Path
 from torch.utils.data import DataLoader, random_split
-from torch.nn import MSELoss
+from torch.nn import MSELoss, SmoothL1Loss
 
 from data_stuff.dataset import SimulationDataset, DatasetExtend1, DatasetExtend2, get_splits
 from data_stuff.utils import SettingsTraining
@@ -16,7 +17,7 @@ from networks.unet3d import UNet3d
 from processing.solver import Solver
 from preprocessing.prepare import prepare_data_and_paths
 from postprocessing.visualization import plot_avg_error_cellwise, visualizations, infer_all_and_summed_pic
-from postprocessing.measurements import measure_loss, save_all_measurements, measure_additional_losses, measure_losses
+from postprocessing.measurements import save_all_measurements, measure_additional_losses
 
 def init_data(settings: SettingsTraining, seed=1):
     if settings.problem in ["2stages", "3d"]:
@@ -35,11 +36,16 @@ def init_data(settings: SettingsTraining, seed=1):
 
     datasets = random_split(dataset, get_splits(len(dataset), split_ratios), generator=generator)
     dataloaders = {}
+    if settings.problem == "3d":
+        batchsize=6
+    else:
+        batchsize=1000
+    
     try:
-        dataloaders["train"] = DataLoader(datasets[0], batch_size=6, shuffle=True, num_workers=0)
-        dataloaders["val"] = DataLoader(datasets[1], batch_size=6, shuffle=True, num_workers=0)
+        dataloaders["train"] = DataLoader(datasets[0], batch_size=batchsize, shuffle=True, num_workers=0)
+        dataloaders["val"] = DataLoader(datasets[1], batch_size=batchsize, shuffle=True, num_workers=0)
     except: pass
-    dataloaders["test"] = DataLoader(datasets[2], batch_size=6, shuffle=True, num_workers=0)
+    dataloaders["test"] = DataLoader(datasets[2], batch_size=batchsize, shuffle=True, num_workers=0)
     return dataset.input_channels, dataloaders
 
 
@@ -64,7 +70,10 @@ def run(settings: SettingsTraining):
 
     solver = None
     if settings.case in ["train", "finetune"]:
-        loss_fn = MSELoss()
+        if settings.problem == "3d":
+            loss_fn = SmoothL1Loss(beta=0.1)
+        else:
+            loss_fn = MSELoss()
         # training
         finetune = True if settings.case == "finetune" else False
         solver = Solver(model, dataloaders["train"], dataloaders["val"], loss_func=loss_fn, finetune=finetune)
@@ -91,36 +100,32 @@ def run(settings: SettingsTraining):
         settings.visualize = True
         which_dataset = "test"
 
-    print(f"Used dataset: {which_dataset}")
-    print(f"Number of datapoints: {len(dataloaders[which_dataset].dataset)}")
     # visualization
     pic_format = "png"
     if settings.visualize:
-        print(f"Start visualization of {which_dataset}")
         start= time.perf_counter()
         visualizations(model, dataloaders[which_dataset], settings.device, plot_path=settings.destination / f"plot_{which_dataset}", amount_datapoints_to_visu=5, pic_format=pic_format)
         times[f"avg_inference_time of {which_dataset}"], summed_error_pic = infer_all_and_summed_pic(model, dataloaders[which_dataset], settings.device)
         plot_avg_error_cellwise(dataloaders[which_dataset], summed_error_pic, {"folder" : settings.destination, "format": pic_format})
-        print("Visualization finished\n")
         times["duration of visualization in seconds"] = time.perf_counter()-start
+        print("Visualizations finished")
 
     #measurements
     print(f"Start measurements of {which_dataset}")
     start= time.perf_counter()
     times[f"avg_inference_time of {which_dataset}"], summed_error_pic = infer_all_and_summed_pic(model, dataloaders[which_dataset], settings.device)
-    errors=measure_losses(model, dataloaders[which_dataset], settings)
-    errors={**errors, **measure_additional_losses(model, dataloaders[which_dataset], summed_error_pic, settings)}
+    errors=measure_additional_losses(model, dataloaders[which_dataset], settings, summed_error_pic)
     times[f"duration of measurement in seconds"]=time.perf_counter()-start
     print("Measurements finished\n")
 
-    save_all_measurements(settings, len(dataloaders[which_dataset].dataset), times, solver, errors)
-    print(f"Whole process including visualization and measurements took {(time.perf_counter()-times['time_begin'])//60} minutes {np.round((time.perf_counter()-times['time_begin'])%60, 1)} seconds\nOutput in {settings.destination.parent.name}/{settings.destination.name}\n")
+    save_all_measurements(model, settings, len(dataloaders[which_dataset].dataset), times, solver, errors)
+    print(f"Whole process took {(times['time_end']-times['time_begin'])//60} minutes {np.round((times['time_end']-times['time_begin'])%60, 1)} seconds\nOutput in {settings.destination.parent.name}/{settings.destination.name}")
 
     return model
 
 def save_inference(model_name:str, in_channels: int, settings: SettingsTraining):
     # push all datapoints through and save all outputs
-    print("Start inference...")
+    print("Start inference...\n")
     time_start = time.perf_counter()
     if settings.problem == "2stages":
         model = UNet(in_channels=in_channels).float()
@@ -164,13 +169,14 @@ def save_inference(model_name:str, in_channels: int, settings: SettingsTraining)
         torch.save(y_out, data_dir / "Outputs" / datapoint.name)
         counter+=1
 
-    print(f"Model loading took {time_model} seconds")
-    print(f"Number of datapoints: {counter}")
-    print(f"Averaged inference time of one datapoint: {time_infer/counter} seconds")
-    print(f"Averaged loading and transfer time of one datapoint: {time_load_and_transfer/counter} seconds")
-    print(f"Whole process of inference took {time.perf_counter() - time_start} seconds")
-
-    print(f"Inference finished, outputs saved in {data_dir / 'Outputs'}")
+    with open(Path.cwd() / "runs" / settings.destination / "measurements_inference.yaml", "w") as f:
+        f.write(f"Model loading took {time_model} seconds\n")
+        f.write(f"Number of datapoints: {counter}\n")
+        f.write(f"Averaged inference time of one datapoint: {time_infer/counter} seconds\n")
+        f.write(f"Averaged loading and transfer time of one datapoint: {time_load_and_transfer/counter} seconds\n")
+        f.write(f"Whole process of inference took {time.perf_counter() - time_start} seconds\n")
+        f.write(f"Outputs saved in {data_dir / 'Outputs'}")
+    print(f"Inference finished. Outputs saved in {data_dir / 'Outputs'}")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING)
@@ -187,7 +193,7 @@ if __name__ == "__main__":
     parser.add_argument("--case_2hp", type=bool, default=False)
     parser.add_argument("--visualize", type=bool, default=False)
     parser.add_argument("--save_inference", type=bool, default=False)
-    parser.add_argument("--problem", type=str, choices=["2stages", "allin1", "extend1", "extend2", "3d",], default="extend1")
+    parser.add_argument("--problem", type=str, choices=["2stages", "allin1", "extend1", "extend2", "3d",], default="2stages")
     parser.add_argument("--notes", type=str, default="")
     parser.add_argument("--len_box", type=int, default=256)
     parser.add_argument("--skip_per_dir", type=int, default=256)
